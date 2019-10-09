836d00e87753db28a157986bd1a09afc80902bf1
donovanrichardson <donovanspqa@gmail.com>
2019-10-09 12:40:43 -0400

    created subheadings for website

--- RLexicon/Sep12.R
+++ RLexicon/Sep12.R
@@ -7,12 +7,19 @@ require(xml2) #read_html
 
 #process wikipedia pages from a OneTab link in order to send them into a database and perform tfidf on them.
 
-NamesRevsPurps <- function(url, purpose){#url is a OneTab url that has all the wikipedia links. purpose is a string that will go to the purpose column in MySQL for each document represented by the links in the OneTab url. In next iteration I will make it so that you can use any list of links in HTML, or any list of links.
+NamesRevsPurps <- function(url, purpose){#url is a OneTab url that has all the wikipedia links. purpose is a string that will go to the purpose column in MySQL for each document represented by the links in the OneTab url. In later iteration I will make it so that you can use any list of links in HTML, or any list of links.
   
   revs <- function(name, lang){ #GET requires httr
     request<-GET(paste0("https://", lang, ".wikipedia.org/w/api.php?action=query&prop=revisions&titles=",name,"&rvprop=ids&format=xml")) #gets the wiki page
     parsedReq<-xmlParse(content(request,"text")) #xml
     revresult <- xpathSApply(parsedReq,"string(//rev/@revid)") #gets the rev number
+    revresult
+  }
+  
+  titles <- function(name, lang){ #like revs but for getting titles. especially important for non-latin alphabets
+    request<-GET(paste0("https://", lang, ".wikipedia.org/w/api.php?action=query&prop=revisions&titles=",name,"&rvprop=ids&format=xml")) #gets the wiki page
+    parsedReq<-xmlParse(content(request,"text")) #xml
+    revresult <- xpathSApply(parsedReq,"string(//page/@title)") #gets the rev number
     revresult
   }
   
@@ -23,10 +30,12 @@ NamesRevsPurps <- function(url, purpose){#url is a OneTab url that has all the w
   names<-gsub("h.*?i/([^/]*?)$","\\1",linkFormat) #gets wikipage names #please the names have to not be random html characters. use the api please #this involves getting the random page xpath for each locale #lol put that in schema too
   langs <- gsub("h.*?//(.*?)[.]wiki.*","\\1",linkFormat) #gets wikipage langs
   revisions<-vector()
+  newnames<-vector()
   for (i in 1:length(names)){
     revisions[i] <- revs(names[i], langs[i])
+    newnames[i]<- titles(names[i], langs[i])
   }
-  nameslangs <- cbind(names,revisions,langs,purpose)
+  nameslangs <- cbind(newnames,revisions,langs,purpose)
   dfnamlang<- data.frame(nameslangs)
   rownames(dfnamlang)<-c()
   dfnamlang
@@ -37,11 +46,13 @@ wikiProcess <- function(oldid, lang, db){#oldid must be a char
   wikipage <- htmlParse(wikiquest)#xml #gets html content of wikipage
   wikiContent <- "" #placeholder
   xPathIndex <- 1 #decides which row will be taken for xpath
-  xPathMax <- 100 #actually it equals the number of xpaths in the schema for the lang #this function needs db
+  xPathMax <- dbGetQuery(db, sprintf("select sum(1) from xpath where lang_id = \"%s\"",lang))[1,1]
+  xPathCandidates <- dbGetQuery(db, sprintf("select expression from xpath where lang_id = \"%s\"", lang), n=-1)$expression
   while (wikiContent == "" & xPathIndex <= xPathMax){
-    wikiXPath <- #NEED DB CONNECTION where xpaths can be obtained#the xPathIndex is used here
+    wikiXPath <- xPathCandidates[xPathIndex]
     wikidesired <- xpathSApply(wikipage, wikiXPath ,toString.XMLNode)
     wikiContent<-tolower(htm2txt(wikidesired))#htm2text #lowercase formatting
+    xPathIndex<- xPathIndex + 1
   }
   if (wikiContent == ""){ #throughout this function, is it better to say if string size is 0?
     stop("xpath") #provides error for correct handling in later function
@@ -66,13 +77,14 @@ continuance<- function(charvector){
   index<-1
   for (c in charvector){
     newdf <- addUnder100(c, newdf) #omg this is why my function is so slow fml. What kind of ridiculous n^2 recursion is this ffs
+    #https://stackoverflow.com/questions/11486369/growing-a-data-frame-in-a-memory-efficient-manner
   }
   newdf
 }
 
 putIntoDB <- function(name="testSep13", rev=0, purp="test", content="", mydb, lang="en", error = F){
-  print(fetch(dbSendQuery(mydb, sprintf("call rev_doc_add(\"%s\",\"%s\",\"%s\",\"%s\",\"%s\")",name, rev, purp, lang, error)),n=-1))#Add error to this procedure, as well as to the unique of the table
-  #nothing else happens if there is error
+  print(fetch(dbSendQuery(mydb, sprintf("call rev_doc_add(\"%s\",\"%s\",\"%s\",\"%s\",\"%s\")",name, rev, purp, lang, error)),n=-1))#Add error to this procedure, as well as to the unique of the table DONE
+  #nothing else happens if there is error 
   if (!error){
     splitContent<-
       if (lang=="ja" | lang == "zh"){#potentially other varieties of chinese. Need to handle non-western punctuation in the future. 
@@ -83,7 +95,6 @@ putIntoDB <- function(name="testSep13", rev=0, purp="test", content="", mydb, la
     splitWithoutBlanks <- splitContent[splitContent!=""]#look to see if whitespace also exists, especially in zh and ja
     contentWithConts <- continuance(splitWithoutBlanks) #fix this hot garbage
     rownum <- nrow(contentWithConts)
-    print(fetch(dbSendQuery(mydb, sprintf("call rev_doc_add(\"%s\",\"%s\",\"%s\", \"%s\")",name, rev, purp, lang)),n=-1))#Add error to this procedure #adds wikipage to db
     while(dbMoreResults(mydb)){#don't know how this would happen if i fetched -1 before
       throwawayResult<-fetch(dbNextResult(mydb), n=-1) #in case there are just random uncollected results
     }
@@ -95,8 +106,8 @@ putIntoDB <- function(name="testSep13", rev=0, purp="test", content="", mydb, la
         anotherResult<-dbNextResult(mydb)#throwaway. might be a duplicate of throwawayResult above, or maybe they're both necessary
       }
       eachquery<-dbSendQuery(mydb, paste0("call textstring_and_word_add(\"",word,"\")")) #adds individual word to database, with doc being the last added doc
-      id <- dbGetQuery(mydb, "select last_insert_id();")[1,1]# the purpose of this is similar to dbNextResult
-      updatequery<- dbSendQuery(mydb,paste0("update textstring set left_continuance=",left,",right_continuance=",right," where word_id=",id))
+      id <- dbGetQuery(mydb, "select last_insert_id();")[1,1]# the purpose of this is similar to dbNextResult  #in theory, is it possible that this would not yield the id that i want?
+      updatequery<- dbSendQuery(mydb,paste0("update textstring set left_continuance=",left,",right_continuance=",right," where word_id=",id))#i might be able to make this more efficient by only calling when necessary
       #tbh it might be appropriate to do contentWithConts as a temp table in mysql bc that would do wonders for performance.
     }
     print(paste("last word added into", name, "doc:")) 
@@ -117,7 +128,7 @@ NamesRevsPurpsDB <- function(url, purpose, db){
       putIntoDB(name = thisname, rev = thisrev, purp=thispurp, content=thiscontent, lang=thislang, mydb = db)#puts wikipage content into DB according to name, revision, purpose, and language
       print(paste(thisname, thisrev, "logged into DB."))#the document has been completely logged into db, according to prev. function.
     }, error = function(e) {
-      if (e$message == "xpath"){ #other errors won't be handled lol
+      if (e$message == "xpath"){ #other errors won't be handled 
         putIntoDB(name = thisname, rev = thisrev, purp=thispurp, lang=thislang, mydb = db, error = T)#allows for logging of xpath error into db
         warning(sprintf("xpath unable to retrieve content for %s %s ; error logged in database", thisname, thisrev))
       } else{
@@ -137,7 +148,7 @@ tfidfCSV <- function(url, purpose, username, pw){
     name <- df[i,2]
     rev <- df[i,4]
     purp <-df[i,5]
-    tfidf <- fetch(dbSendQuery(db, paste0("call tfidf(",doc_id,")")), n=-1)#ALSO INCLUDE EXISTS_ERROR
+    tfidf <- fetch(dbSendQuery(db, paste0("call tfidf(",doc_id,")")), n=-1)#procedure now also includes exists_error DONE
     #worth mentioning lots of times, that integers like doc_id max out at around 2 billion in MySQL. The highest ints so far are ~900 million
     while(dbMoreResults(db)){
       res1<- dbNextResult(db)
