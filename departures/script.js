bda6ae4550cea38a46d328490f4c18678beeaf63
donovanrichardson <donovanspqa@gmail.com>
2020-02-06 16:49:52 -0500

    still fixing... want it to look good

--- departures/script.js
+++ departures/script.js
@@ -46,7 +46,7 @@ function maketitle(text){
   var title = document.createElement("h1");
   title.textContent = text;
   title.style.textAlign = "center";
-  document.getElementById("root").appendChild(title);
+  document.getElementById("root").insertBefore(window.canvas);
 }
 
 function fxn() {
